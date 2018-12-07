88bacfd45c25e793cd36a3479fa49295f1542577
Kulikov Evginy <zhek9kulikov@gmail.com>
2018-12-07 12:02:36 +0300

    new images

--- projectone/urls.py
+++ projectone/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^base$', views.base_template, name='base_template'),
 ]
--- projectone/views.py
+++ projectone/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index/index.html', {})
+
+def base_template(request):
+    return render(request, 'base_template/base_template.html', {})
