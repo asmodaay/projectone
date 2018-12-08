111060fb0c0d1c3b99cdcfce02d8b275668a87d3
Kulikov Evginy <zhek9kulikov@gmail.com>
2018-12-08 20:15:43 +0300

    changed directory + add post_detail

--- projectone/urls.py
+++ projectone/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^posts_list$', views.posts_list, name='posts_list'),
     url(r'^base$', views.base_template, name='base_template'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- projectone/views.py
+++ projectone/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 
 def index(request):
-    return render(request, 'index/index.html', {})
+    return render(request, 'projectone/index.html', {})
 
 def posts_list(request):
 	posts = Post.objects.all()
-	return render(request, 'posts_list/posts_list.html', {'posts': posts})
+	return render(request, 'projectone/posts_list.html', {'posts': posts})
 
 def base_template(request):
-	return render(request, 'base_template/base_template.html', {})
-    
+	return render(request, 'projectone/base_template.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'projectone/post_detail.html', {'post': post})
