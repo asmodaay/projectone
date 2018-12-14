e87d0be452b89f8ff45ae2e8d9a4ee8ded403237
Kulikov Evginy <zhek9kulikov@gmail.com>
2018-12-14 18:17:33 +0300

    db del

--- projectone/urls.py
+++ projectone/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^posts_list/(?P<pk>\d+)/$', views.posts_list, name='posts_list'),
+    url(r'^posts_list/$', views.posts_list, name='posts_list'),
     url(r'^base$', views.base, name='base'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
